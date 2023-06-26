e217e2ff656af6c00092c0554df9274bcb1b8052
azure <1666534783@qq.com>
2023-06-27 00:36:28 +0800

    add probability

--- pytorch/2-预备知识/2-6_概率.py
+++ pytorch/2-预备知识/2-6_概率.py
@@ -3,15 +3,15 @@ from torch.distributions import multinomial
 from d2l import torch as d2l
 
 # 模拟投骰子的过程，对其进行抽样
-fair_porbs = torch.ones([6]) / 6 # 六个面概率相等
-print(multinomial.Multinomial(1,fair_porbs).sample()) # 以相等的概率进行一次抽样
-print(multinomial.Multinomial(10,fair_porbs).sample()) # 以相等的概率进行十次抽样
-counts = multinomial.Multinomial(1000,fair_porbs).sample()
+fair_probs = torch.ones([6]) / 6 # 六个面概率相等
+print(multinomial.Multinomial(1,fair_probs).sample()) # 以相等的概率进行一次抽样
+print(multinomial.Multinomial(10,fair_probs).sample()) # 以相等的概率进行十次抽样
+counts = multinomial.Multinomial(1000,fair_probs).sample()
 print(counts) # 以相等的概率进行1000次抽样
 print(counts / 1000) # 求出每个面的概率'
 
 # 进行500次实验，每次实验10次抽样
-counts = multinomial.Multinomial(10, fair_porbs).sample((500,))
+counts = multinomial.Multinomial(10, fair_probs).sample((500,))
 cum_counts = counts.cumsum(dim=0)
 print(cum_counts) # 每次实验后的累计次数
 estimates = cum_counts / cum_counts.sum(dim=1, keepdims=True)
