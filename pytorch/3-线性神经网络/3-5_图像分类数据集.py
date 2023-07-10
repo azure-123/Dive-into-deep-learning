05445b24d26de850558eb4f63c075188f7501f39
azure <2920410449@qq.com>
2023-07-10 16:43:20 +0800

    modify image classification dataset

--- pytorch/3-线性神经网络/3-5_图像分类数据集.py
+++ pytorch/3-线性神经网络/3-5_图像分类数据集.py
@@ -2,3 +2,74 @@ import torch
 import torchvision
 from torch.utils import data
 from torchvision import transforms
+from d2l import torch as d2l
+
+# 使用svg显示图片
+d2l.use_svg_display()
+
+# 通过ToTensor实例将图像数据从PIL类型变换成32位浮点数格式，
+# 并除以255使得所有像素的数值均在0～1之间
+# trans = transforms.ToTensor()
+trans = transforms.ToTensor()
+mnist_train = torchvision.datasets.FashionMNIST(root="../data", train=True, transform=trans, download=False)
+mnist_test = torchvision.datasets.FashionMNIST(root="../data", train=False, transform=trans, download=False)
+
+
+print(len(mnist_train),len(mnist_test))
+print(mnist_train[0][0].shape)
+
+def get_fashion_mnist_labels(labels):  #@save
+    """返回Fashion-MNIST数据集的文本标签"""
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):  #@save
+    """绘制图像列表"""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = d2l.plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        if torch.is_tensor(img):
+            # 图片张量
+            ax.imshow(img.numpy())
+        else:
+            # PIL图片
+            ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
+
+# 加载18张图片，查看其中的数据集
+X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
+show_images(X.reshape(18, 28, 28), num_rows=2, num_cols=9, titles=get_fashion_mnist_labels(y))
+d2l.plt.show()
+
+# 获取一小批数据
+batch_size=256
+
+def get_dataloader_workers():  #@save
+    """使用4个进程来读取的数据"""
+    return 4
+
+train_iter = data.DataLoader(mnist_train,batch_size=batch_size, shuffle=True, num_workers=get_dataloader_workers())
+
+# 计算读取的时间
+timer = d2l.Timer()
+for X, y in train_iter:
+    continue
+print(f'{timer.stop():.2f} sec')
+
+def load_data_fashion_mnist(batch_size, resize=None):  #@save
+    """下载Fashion-MNIST数据集，然后将其加载到内存中"""
+    trans = []
+    if resize:
+        trans.append(transforms.Resize(resize))
+    trans.append(transforms.ToTensor())
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root="../data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(root="../data", train=False, transform=trans, download=True)
+    return [data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size=batch_size, shuffle=True, num_workers=get_dataloader_workers())]
