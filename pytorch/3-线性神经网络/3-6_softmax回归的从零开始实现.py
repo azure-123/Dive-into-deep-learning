61df1b811e589988fb336dea7462b65994b2f794
azure <1666534783@qq.com>
2023-07-11 14:48:55 +0800

    modify softmax regression scratch

--- pytorch/3-线性神经网络/3-6_softmax回归的从零开始实现.py
+++ pytorch/3-线性神经网络/3-6_softmax回归的从零开始实现.py
@@ -28,3 +28,16 @@ print(X_prob.sum(dim=1))
 def net(X):
     return softmax(torch.matmul(X.reshape(-1, W.shape[0]), W) + b)
 
+# 将y作为y_hat中的概率索引
+y = torch.tensor([0, 2])
+y_hat = torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
+print(y_hat[[0, 1], y])
+
+# 定义损失函数，为交叉熵函数
+def cross_entropy(y_hat, y):
+    return -torch.log(y_hat[range(len(y_hat)), y]) # y_hat取其每个样本的索引，y为每个样本的正确分类，将这个正确分类套到y_hat当中，看这个分类在y_hat当中的概率为多少
+
+print(cross_entropy(y_hat, y)) # 第一个正确分类是第0个，但是y_hat中其概率特别小，说明预测错误，返回值很大；第二个预测正确了，返回值很小
+
+
+
