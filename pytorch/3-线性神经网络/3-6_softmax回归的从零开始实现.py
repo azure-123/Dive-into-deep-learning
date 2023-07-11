1a29a2cc711054742bd03f45a5f3bb7352d89066
azure <2920410449@qq.com>
2023-07-11 14:14:54 +0800

    modify softmax regression scratch

--- pytorch/3-线性神经网络/3-6_softmax回归的从零开始实现.py
+++ pytorch/3-线性神经网络/3-6_softmax回归的从零开始实现.py
@@ -1,2 +1,25 @@
 import torch
 from IPython import display
+from d2l import torch as d2l
+
+# 加载训练和测试数据集
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
+
+# 初始化权重和偏置参数
+num_inputs = 28 * 28
+num_outputs = 10
+W = torch.normal(0, 0.01, (num_inputs, 10), requires_grad=True)
+b = torch.zeros(10, requires_grad=True)
+
+# 定义softmax函数
+def softmax(X):
+    X_exp = torch.exp(X)
+    partition = X_exp.sum(1, keepdim=True)
+    return torch.exp(X) / partition
+
+# 检测softmax函数的正确性
+X = torch.normal(0, 1, (2, 5))
+X_prob = softmax(X)
+print(X_prob)
+print(X_prob.sum(dim=1))
