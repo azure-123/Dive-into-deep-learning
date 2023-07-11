81aa44826e2ea2256f38aa98578d512d165377a8
azure <1666534783@qq.com>
2023-07-11 17:26:24 +0800

    modify softmax regression concise

--- pytorch/3-线性神经网络/3-7_softmax回归的简洁实现.py
+++ pytorch/3-线性神经网络/3-7_softmax回归的简洁实现.py
@@ -5,20 +5,25 @@ from d2l import torch as d2l
 batch_size = 256
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
 
-# 创建模型
+# 创建模型，在线性层之前定义展平层，调整网络输入的形状
 net = nn.Sequential(nn.Flatten(), nn.Linear(28 * 28, 10))
 
 # 初始化权重
-def init_weight(m):
+def init_weights(m):
     if isinstance(m, nn.Linear):
         nn.init.normal_(m.weight, std=0.01)
+net.apply(init_weights)
 
 # 定义损失函数（交叉熵函数）
-loss = nn.CrossEntropyLoss(reduce='None')
+loss = nn.CrossEntropyLoss(reduce='mean')
 
 # 算法优化器
 trainer = torch.optim.SGD(net.parameters(),lr=0.1)
 
 #开始训练
 num_epochs = 10
-d2l.train_epoch_ch3(net, train_iter=train_iter,loss=loss, updater=trainer)
+d2l.train_ch3(net, train_iter=train_iter, test_iter=test_iter, loss=loss, num_epochs=num_epochs, updater=trainer)
+d2l.plt.show()
+
+d2l.predict_ch3(net, test_iter)
+d2l.plt.show()
