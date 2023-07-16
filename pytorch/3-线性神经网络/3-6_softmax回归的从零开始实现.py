d5e73dfe29d5001324ff3d21c62c33a76303820d
azure <1666534783@qq.com>
2023-07-16 11:02:34 +0800

    modify softmax regression scratch

--- pytorch/3-线性神经网络/3-6_softmax回归的从零开始实现.py
+++ pytorch/3-线性神经网络/3-6_softmax回归的从零开始实现.py
@@ -110,7 +110,8 @@ def predict_ch3(net, test_iter, n=6):  #@save
         trues = d2l.get_fashion_mnist_labels(y)
         preds = d2l.get_fashion_mnist_labels(net(X).argmax(dim=1))
         titles = [true + '\n' + pred for true, pred in zip(trues, preds)]
-        d2l.show_images(X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
+        d2l.show_images(
+        X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
 
 predict_ch3(net=net,test_iter=test_iter)
 
