d17ae189d890830febb9413b5bef0b3ef5903eb7
azure <1666534783@qq.com>
2023-07-16 11:45:55 +0800

    modify multilayer perceptrons

--- pytorch/4-多层感知机/4-1_多层感知机.py
+++ pytorch/4-多层感知机/4-1_多层感知机.py
@@ -1,4 +1,34 @@
 import torch
-from torch import nn
 from d2l import torch as d2l
 
+""" 常见激活函数"""
+
+# ReLU函数
+x = torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
+y = torch.relu(x)
+d2l.plot(x.detach(), y.detach(), 'x', 'ReLU(x)', figsize=(5, 2.5))
+d2l.plt.show()
+# ReLU函数的导数
+y.backward(torch.ones_like(x), retain_graph=True)
+d2l.plot(x.detach(), x.grad, 'x', 'grad of relu', figsize=(5, 2.5))
+d2l.plt.show()
+
+# sigmoid函数
+y = torch.sigmoid(x)
+d2l.plot(x.detach(), y.detach(), 'x', 'sigmoid(x)', figsize=(5, 2.5))
+d2l.plt.show()
+# sigmoid函数的导数
+x.grad.data.zero_()
+y.backward(torch.ones_like(x), retain_graph=True)
+d2l.plot(x.detach(), x.grad, 'x', 'grad of sigmoid', figsize=(5, 2.5))
+d2l.plt.show()
+
+# tanh函数
+y = torch.tanh(x)
+d2l.plot(x.detach(), y.detach(), 'x', 'tanh(x)', figsize=(5, 2.5))
+d2l.plt.show()
+# tanh函数的导数
+x.grad.data.zero_()
+y.backward(torch.ones_like(x), retain_graph=True)
+d2l.plot(x.detach(), x.grad, 'x', 'grad of sigmoid', figsize=(5, 2.5))
+d2l.plt.show()
