d36b3d36d71730905290d31f9ffce6494deb6453
azure <1666534783@qq.com>
2023-07-19 23:04:28 +0800

    modify weight decay

--- pytorch/4-多层感知机/4-5_权重衰减.py
+++ pytorch/4-多层感知机/4-5_权重衰减.py
@@ -2,3 +2,9 @@ import torch
 from torch import nn
 from d2l import torch as d2l
 
+n_train, n_test, num_input, batch_size = 20, 100, 200, 5
+true_w, true_b = torch.ones((num_input, 1)) * 0.01, 0.05
+train_data = d2l.synthetic_data(true_w, true_b, n_train)
+train_iter = d2l.load_array(train_data, batch_size)
+test_data = d2l.synthetic_data(true_w, true_b, n_test)
+test_iter = d2l.load_array(test_data, batch_size, is_train=False)
