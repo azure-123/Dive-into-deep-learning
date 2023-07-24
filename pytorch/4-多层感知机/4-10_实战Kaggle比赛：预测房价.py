a49da1f079dff2251131ea2cad120c6ffc17fc4b
azure <1666534783@qq.com>
2023-07-24 18:00:45 +0800

    modify kaggle house price

--- pytorch/4-多层感知机/4-10_实战Kaggle比赛：预测房价.py
+++ pytorch/4-多层感知机/4-10_实战Kaggle比赛：预测房价.py
@@ -48,3 +48,60 @@ def download_all():  #@save
     """下载DATA_HUB中的所有文件"""
     for name in DATA_HUB:
         download(name)
+
+# 引入需要的包
+import numpy as np
+import pandas as pd
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+# 加载数据集
+DATA_HUB['kaggle_house_train'] = (  #@save
+    DATA_URL + 'kaggle_house_pred_train.csv',
+    '585e9cc93e70b39160e7921475f9bcd7d31219ce')
+
+DATA_HUB['kaggle_house_test'] = (  #@save
+    DATA_URL + 'kaggle_house_pred_test.csv',
+    'fa19780a7b011d9b009e8bff8e99922a8ee2eb90')
+
+train_data = pd.read_csv(download('kaggle_house_train'))
+test_data = pd.read_csv(download('kaggle_house_test'))
+
+print(train_data.shape)
+print(test_data.shape)
+
+# 查看个别特征
+print(train_data.iloc[0:4, [0, 1, 2, 3, -3, -2, -1]])
+
+# 提取有用特征，将id删去，因为其对预测没有太大用处
+all_features = pd.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
+
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+all_features[numeric_features] = all_features[numeric_features].apply(lambda x: (x - x.mean()) / x.std())
+all_features[numeric_features] = all_features[numeric_features].fillna(0)
+
+# 用one_hot方式表示非数值的数据
+all_features = pd.get_dummies(all_features, dummy_na=True)
+# 获取数据集
+n_train = train_data.shape[0]
+train_features = torch.tensor(all_features[:n_train].values, dtype=torch.float32)
+test_features = torch.tensor(all_features[n_train:].values, dtype=torch.float32)
+train_labels = torch.tensor(train_data.SalePrice.values.reshape(-1, 1), dtype=torch.float32)
+
+# 定义损失函数
+loss = nn.MSELoss()
+
+# 定义网络
+in_features = train_features.shape[1]
+def get_net():
+    net = nn.Sequential(nn.Linear(in_features,1))
+    return net
+
+net = get_net()
+
+def loss_rmse(net, features, labels):
+    clipped_preds = torch.clamp(net(features), 1, float('inf'))
+    rmse = torch.sqrt(loss(torch.log(clipped_preds), torch.log(labels)))
+    return rmse.item()
+
