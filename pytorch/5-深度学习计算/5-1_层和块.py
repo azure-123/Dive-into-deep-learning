4e66b6e466468d5907307f4202d6689acd955826
azure <1666534783@qq.com>
2023-07-25 22:52:16 +0800

    modify module construction

--- pytorch/5-深度学习计算/5-1_层和块.py
+++ pytorch/5-深度学习计算/5-1_层和块.py
@@ -8,3 +8,28 @@ net = nn.Sequential(nn.Linear(20, 256),
                     nn.Linear(256, 10))
 X = torch.rand(2, 20)
 print(net(X))
+
+# MLP模块
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__() # 继承父类nn.Module
+        self.hidden = nn.Linear(20, 256) # 隐藏层
+        self.out = nn.Linear(256, 10) # 输出层
+    def forward(self, X):
+        return self.out(F.relu(self.hidden(X)))
+net = MLP()
+print(net(X))
+
+# 顺序块
+class MySequential(nn.Module):
+    def __init__(self, *args):
+        super().__init__()
+        for block in args:
+            self._modules[block] = block
+
+    def forward(self, X):
+        for block in self._modules.values():
+            X = block(X)
+        return X
+net = MySequential(nn.Linear(20, 256), nn.ReLU(), nn.Linear(256, 10))
+print(net(X))
