0380fb2285198c3ce2a7eb9899cfbd1569d6d91b
azure <1666534783@qq.com>
2023-07-25 22:39:50 +0800

    modify model construction

--- pytorch/5-深度学习计算/5-1_层和块.py
+++ pytorch/5-深度学习计算/5-1_层和块.py
@@ -1,3 +1,10 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
 
+# MLP结构
+net = nn.Sequential(nn.Linear(20, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 10))
+X = torch.rand(2, 20)
+print(net(X))
