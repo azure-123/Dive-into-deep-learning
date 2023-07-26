5daa83a3229fa5d92df69b0397de0432a3c7b32e
azure <1666534783@qq.com>
2023-07-26 17:04:42 +0800

    modify module construction

--- pytorch/5-深度学习计算/5-1_层和块.py
+++ pytorch/5-深度学习计算/5-1_层和块.py
@@ -33,3 +33,35 @@ class MySequential(nn.Module):
         return X
 net = MySequential(nn.Linear(20, 256), nn.ReLU(), nn.Linear(256, 10))
 print(net(X))
+
+# 指定常数参数
+class FixedHiddenMLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.rand_weight = torch.rand((20, 20), requires_grad=False)
+        self.linear = nn.Linear(20, 20)
+    
+    def forward(self, X):
+        X = self.linear(X)
+        X = F.relu(torch.mm(X, self.rand_weight) + 1)
+        X = self.linear(X)
+        if X.abs().sum() > 1:
+            X /= 2
+        return X.sum()
+net = FixedHiddenMLP()
+print(net(X))
+
+# 混合搭配组合块
+class NestMLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(nn.Linear(20, 64),
+                                 nn.ReLU(),
+                                 nn.Linear(64, 32),
+                                 nn.ReLU())
+        self.linear = nn.Linear(32, 16)
+    
+    def forward(self, X):
+        return self.linear(self.net(X))
+chimera = nn.Sequential(NestMLP(), nn.Linear(16, 20), FixedHiddenMLP())
+print(chimera(X))
