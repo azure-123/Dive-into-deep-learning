306c6a04f9ed939388adebbd3ec8c23525552cdc
azure <1666534783@qq.com>
2023-07-29 23:58:54 +0800

    modify parameters

--- pytorch/5-深度学习计算/5-2_参数管理.py
+++ pytorch/5-深度学习计算/5-2_参数管理.py
@@ -1,8 +1,11 @@
 import torch
 from torch import nn
 
+# 网络结构
 net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 1))
 X = torch.rand(size=(2, 4))
 print(net(X))
+
+# 查看某一层的权重
 print(net[2].state_dict())
 
