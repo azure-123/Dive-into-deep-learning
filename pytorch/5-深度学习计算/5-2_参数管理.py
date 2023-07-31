16cd517122e2a3dbb566a005ddab6ddeb96f4f6f
azure <1666534783@qq.com>
2023-07-31 11:31:37 +0800

    modify parameters

--- pytorch/5-深度学习计算/5-2_参数管理.py
+++ pytorch/5-深度学习计算/5-2_参数管理.py
@@ -12,3 +12,69 @@ print(net[2].state_dict())
 print(type(net[2].bias))
 print(net[2].bias)
 print(net[2].bias.data)
+# 尚未进行求导，因此没有grad
+print(net[2].weight.grad == None)
+# 一次性访问所有参数
+print(*[(name, param.shape) for name, param in net[0].named_parameters()])
+print(*[(name, param.shape) for name, param in net.named_parameters()])
+print(net.state_dict()['2.bias'].data)
+
+# 从嵌套块中获取参数
+def block1():
+    return nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 4), nn.ReLU())
+
+def block2():
+    net = nn.Sequential()
+    for i in range(4):
+        net.add_module(f'block{i}', block1())
+    return net
+rgnet = nn.Sequential(block2(), nn.Linear(4, 1))
+print(rgnet(X))
+print(rgnet)
+print(rgnet[0][1][0].bias.data)
+
+# 内置初始化
+def init_normal(m): # 初始化为正态分布
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, mean=0, std=0.01)
+        nn.init.zeros_(m.bias)
+net.apply(init_normal)
+print(net[0].weight.data[0], net[0].bias.data[0])
+
+def init_constant(m): # 初始化为常数
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 1)
+        nn.init.zeros_(m.bias)
+net.apply(init_constant)
+print(net[0].weight.data[0], net[0].bias.data[0])
+# 对某些块应用初始化方法
+def init_xavier(m):
+    if type(m) == nn.Linear:
+        nn.init.xavier_uniform_(m.weight)
+def init_42(m):
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 42)
+net[0].apply(init_xavier)
+net[2].apply(init_42)
+print(net[0].weight.data[0])
+print(net[2].weight.data)
+
+# 自定义初始化
+def my_init(m):
+    if type(m) == nn.Linear:
+        print('Init', *[(name, param.shape) for name, param in m.named_parameters()][0])
+        nn.init.uniform_(m.weight, -10, 10)
+        m.weight.data *= m.weight.data.abs() >= 5
+net.apply(my_init)
+print(net[0].weight[:2])
+# 直接设置参数
+net[0].weight.data[:] += 1
+net[0].weight.data[0, 0] = 42
+print(net[0].weight.data[0])
+
+# 参数绑定，多个层间共享参数
+shared = nn.Linear(8, 8)
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), shared, nn.ReLU(), shared, nn.Linear(8, 4))
+print(net(X))
+net[2].weight.data[0, 0] = 100
+print(net[2].weight.data[0] == net[4].weight.data[0]) # 两者相等，说明指向同一块内存
