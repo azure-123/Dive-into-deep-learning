fb93d07951360437c734cb1d52e344a0594054ca
azure <1666534783@qq.com>
2023-07-31 17:04:51 +0800

    modify custom layer

---
+++ pytorch/5-深度学习计算/5-4_自定义层.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+# 定义一个层
+class CenteredLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, X):
+        return X - X.mean()
+layer = CenteredLayer()
+print(layer(torch.FloatTensor([1, 2, 3, 4, 5])))
+
+# 将这个层用在网络当中
+net = nn.Sequential(nn.Linear(8, 128), CenteredLayer())
+Y = net(torch.rand(4, 8))
+print(Y.mean())
+
+# 定义一个带参数的层
+class MyLinear(nn.Module):
+    def __init__(self, in_units, units):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(in_units, units))
+        self.bias = nn.Parameter(torch.zeros(units,))
+    def forward(self, X):
+        linear = torch.matmul(X, self.weight.data) + self.bias.data
+        return F.relu(linear)
+dense = MyLinear(5, 3)
+print(dense.weight)
