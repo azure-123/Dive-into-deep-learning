a09246c01afeda3e2a36f9f3cf4616059c160ddc
azure <1666534783@qq.com>
2023-07-31 11:53:35 +0800

    modify deferred init

--- pytorch/5-深度学习计算/5-3_延后初始化.py
+++ pytorch/5-深度学习计算/5-3_延后初始化.py
@@ -1,2 +1,9 @@
 import torch
 from torch import nn
+
+"""延后初始化"""
+net = nn.Sequential(nn.LazyLinear(256), nn.ReLU(), nn.LazyLinear(10))
+print(net[0].weight) # 尚未初始化 
+X = torch.rand(2, 20)
+print(net(X))
+print(net)
