3512af7a3013f5d78b665fa036e7ed874cf23065
azure <1666534783@qq.com>
2023-08-05 16:42:29 +0800

    modify lenet

--- pytorch/6-卷积神经网络/6-6_卷积神经网络（LeNet）.py
+++ pytorch/6-卷积神经网络/6-6_卷积神经网络（LeNet）.py
@@ -12,7 +12,7 @@ net = nn.Sequential(Reshape(),
                     nn.Conv2d(6, 16, kernel_size=5), nn.Sigmoid(),
                     nn.AvgPool2d(kernel_size=2, stride=2),
                     nn.Flatten(),
-                    nn.Linear(16 * 5 * 5, 120), nn.Sigmoid(),
+                    nn.Linear(16 * 5 * 5, 120), nn.ReLU(),
                     nn.Linear(120, 84), nn.Sigmoid(),
                     nn.Linear(84, 10))
 
