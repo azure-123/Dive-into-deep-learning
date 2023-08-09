cb37d5d0d28d8d398e98cad0b7a201133f222972
azure <1666534783@qq.com>
2023-08-09 23:19:52 +0800

    modify googlenet

--- pytorch/7-现代卷积神经网络/7-4_含并行连结的网络（GoogLeNet）.py
+++ pytorch/7-现代卷积神经网络/7-4_含并行连结的网络（GoogLeNet）.py
@@ -3,10 +3,12 @@ from torch import nn
 from torch.nn import functional as F
 from d2l import torch as d2l
 
+
 class Inception(nn.Module):
+    # c1--c4是每条路径的输出通道数
     def __init__(self, in_channels, c1, c2, c3, c4, **kwargs):
         super(Inception, self).__init__(**kwargs)
-        # 线路1，1x1卷积层
+        # 线路1，单1x1卷积层
         self.p1_1 = nn.Conv2d(in_channels, c1, kernel_size=1)
         # 线路2，1x1卷积层后接3x3卷积层
         self.p2_1 = nn.Conv2d(in_channels, c2[0], kernel_size=1)
@@ -15,14 +17,15 @@ class Inception(nn.Module):
         self.p3_1 = nn.Conv2d(in_channels, c3[0], kernel_size=1)
         self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
         # 线路4，3x3最大汇聚层后接1x1卷积层
-        self.p4_1 = nn.MaxPool2d(kernel_size=3)
+        self.p4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
         self.p4_2 = nn.Conv2d(in_channels, c4, kernel_size=1)
 
-    def forward(self, X):
-        p1 = F.relu(self.p1_1(X))
-        p2 = F.relu(self.p2_2(F.relu(self.p2_1(X))))
-        p3 = F.relu(self.p3_2(F.relu(self.p3_1(X))))
-        p4 = F.relu(self.p4_2(self.p4_1(X)))
+    def forward(self, x):
+        p1 = F.relu(self.p1_1(x))
+        p2 = F.relu(self.p2_2(F.relu(self.p2_1(x))))
+        p3 = F.relu(self.p3_2(F.relu(self.p3_1(x))))
+        p4 = F.relu(self.p4_2(self.p4_1(x)))
+        # 在通道维度上连结输出
         return torch.cat((p1, p2, p3, p4), dim=1)
 
 # 五个stage
