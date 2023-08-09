14584e3fd518adeeb830345e18420cf172884ebf
azure <1666534783@qq.com>
2023-08-09 15:43:17 +0800

    modify alexnet

--- pytorch/7-现代卷积神经网络/7-1_深度卷积神经网络（AlexNet）.py
+++ pytorch/7-现代卷积神经网络/7-1_深度卷积神经网络（AlexNet）.py
@@ -35,3 +35,4 @@ batch_size = 128
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
 lr, num_epochs = 0.01, 10
 d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
+d2l.plt.show()
