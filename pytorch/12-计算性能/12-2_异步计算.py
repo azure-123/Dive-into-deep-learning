24905bf883327b1238111453bcdafedcfb6017fe
azure <1666534783@qq.com>
2023-08-14 16:50:44 +0800

    modify async computation

--- pytorch/12-计算性能/12-2_异步计算.py
+++ pytorch/12-计算性能/12-2_异步计算.py
@@ -4,3 +4,24 @@ import numpy
 import torch
 from torch import nn
 from d2l import torch as d2l
+
+# 通过后端异步处理
+device = d2l.try_gpu()
+a = torch.randn(size=(1000, 1000), device=device)
+b = torch.mm(a, a)
+
+with d2l.Benchmark('numpy'):
+    for _ in range(10):
+        a = numpy.random.normal(size=(1000, 1000))
+        b = numpy.dot(a, a)
+
+with d2l.Benchmark('torch'):
+    for _ in range(10):
+        a = torch.randn(size=(1000, 1000), device=device)
+        b = torch.mm(a, a)
+
+with d2l.Benchmark():
+    for _ in range(10):
+        a = torch.randn(size=(1000, 1000), device=device)
+        b = torch.mm(a, a)
+    torch.cuda.synchronize(device)
