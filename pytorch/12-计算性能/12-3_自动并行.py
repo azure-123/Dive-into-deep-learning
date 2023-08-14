1a11e4b3016a4cf6bddbf3ac57e7f7a30cb2e748
azure <1666534783@qq.com>
2023-08-14 17:01:46 +0800

    modify auto parallelism

---
+++ pytorch/12-计算性能/12-3_自动并行.py
@@ -0,0 +1,43 @@
+import torch
+from d2l import torch as d2l
+
+devices = d2l.try_all_gpus()
+def run(x):
+    return [x.mm(x) for _ in range(50)]
+
+x_gpu1 = torch.rand(size=(4000, 4000), device=devices[0])
+x_gpu2 = torch.rand(size=(4000, 4000), device=devices[1])
+
+run(x_gpu1)
+run(x_gpu2)  # 预热设备
+torch.cuda.synchronize(devices[0])
+torch.cuda.synchronize(devices[1])
+
+with d2l.Benchmark('GPU1 time'):
+    run(x_gpu1)
+    torch.cuda.synchronize(devices[0])
+
+with d2l.Benchmark('GPU2 time'):
+    run(x_gpu2)
+    torch.cuda.synchronize(devices[1])
+
+with d2l.Benchmark('GPU1 & GPU2'):
+    run(x_gpu1)
+    run(x_gpu2)
+    torch.cuda.synchronize()
+
+def copy_to_cpu(x, non_blocking=False):
+    return [y.to('cpu', non_blocking=non_blocking) for y in x]
+
+with d2l.Benchmark('在GPU1上运行'):
+    y = run(x_gpu1)
+    torch.cuda.synchronize()
+
+with d2l.Benchmark('复制到CPU'):
+    y_cpu = copy_to_cpu(y)
+    torch.cuda.synchronize()
+
+with d2l.Benchmark('在GPU1上运行并复制到CPU'):
+    y = run(x_gpu1)
+    y_cpu = copy_to_cpu(y, True)
+    torch.cuda.synchronize()
