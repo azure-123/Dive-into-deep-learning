861bf1196722a73d59e2141fc61200c0c4c0fc7d
azure <1666534783@qq.com>
2023-08-17 17:17:45 +0800

    modify fine tuning

--- pytorch/13-计算机视觉/13-2_微调.py
+++ pytorch/13-计算机视觉/13-2_微调.py
@@ -3,6 +3,7 @@ import torch
 import torchvision
 from torch import nn
 from d2l import torch as d2l
+from torch.utils import data
 
 #@save
 d2l.DATA_HUB['hotdog'] = (d2l.DATA_URL + 'hotdog.zip',
@@ -16,3 +17,54 @@ test_imgs = torchvision.datasets.ImageFolder(os.path.join(data_dir, 'test'))
 hotdogs = [train_imgs[i][0] for i in range(8)]
 not_hotdogs = [train_imgs[-i - 1][0] for i in range(8)]
 d2l.show_images(hotdogs + not_hotdogs, 2, 8, scale=1.4)
+d2l.plt.show()
+
+# 使用RGB通道的均值和标准差，以标准化每个通道
+normalize = torchvision.transforms.Normalize(
+    [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+train_augs = torchvision.transforms.Compose([torchvision.transforms.RandomResizedCrop(224),
+                                             torchvision.transforms.RandomHorizontalFlip(),
+                                             torchvision.transforms.ToTensor(),
+                                             normalize])
+test_augs = torchvision.transforms.Compose([torchvision.transforms.Resize([256, 256]),
+                                            torchvision.transforms.CenterCrop(224),
+                                            torchvision.transforms.ToTensor(),
+                                            normalize
+                                            ])
+
+# 定义和初始化模型
+pretrained_net = torchvision.models.resnet18(pretrained=True)
+print(pretrained_net.fc)
+# 微调模型
+finetune_net = torchvision.models.resnet18(pretrained=True)
+finetune_net.fc = nn.Linear(finetune_net.fc.in_features, 2)
+nn.init.xavier_uniform_(finetune_net.fc.weight)
+
+# 如果param_group=True，输出层中的模型参数将使用十倍的学习率
+def train_fine_tuning(net, learning_rate, batch_size=128, num_epochs=5, param_group=True):
+    train_iter = torch.utils.data.DataLoader(torchvision.datasets.ImageFolder(
+        os.path.join(data_dir, 'train'), transform=train_augs),
+        batch_size=batch_size, shuffle=True)
+    test_iter = torch.utils.data.DataLoader(torchvision.datasets.ImageFolder(
+        os.path.join(data_dir, 'test'), transform=test_augs),
+        batch_size=batch_size)
+    devices = d2l.try_all_gpus()
+    loss = nn.CrossEntropyLoss(reduction='none')
+    if param_group:
+        params_1x = [param for name, param in net.named_parameters() if name not in ['fc.weight', 'fc.bias']]
+        trainer = torch.optim.SGD([{'params': params_1x},
+                                   {'params': net.fc.parameters(), 'lr': learning_rate * 10}],
+                                lr=learning_rate, weight_decay=0.001)
+    else:
+        trainer = torch.optim.SGD(net.parameters(), lr=learning_rate, weight_decay=0.001)
+    d2l.train_ch13(net, train_iter, test_iter, loss, trainer, num_epochs, devices)
+
+# 使用较小学习率，使用预训练模型并微调
+train_fine_tuning(finetune_net, 5e-5)
+# 和未经过预训练的模型做对比
+scratch_net = torchvision.models.resnet18()
+scratch_net.fc = nn.Linear(scratch_net.fc.in_features, 2)
+train_fine_tuning(scratch_net, 5e-4, param_group=False)
+
+d2l.plt.show()
