83eb4b7ff63878bf32d8e0ca5661df575564728b
azure <1666534783@qq.com>
2023-08-17 16:44:16 +0800

    modify fine tuning

--- pytorch/13-计算机视觉/13-2_微调.py
+++ pytorch/13-计算机视觉/13-2_微调.py
@@ -1,4 +1,18 @@
+import os
 import torch
+import torchvision
 from torch import nn
 from d2l import torch as d2l
 
+#@save
+d2l.DATA_HUB['hotdog'] = (d2l.DATA_URL + 'hotdog.zip',
+                         'fba480ffa8aa7e0febbb511d181409f899b9baa5')
+
+data_dir = d2l.download_extract('hotdog')
+
+train_imgs = torchvision.datasets.ImageFolder(os.path.join(data_dir, 'train'))
+test_imgs = torchvision.datasets.ImageFolder(os.path.join(data_dir, 'test'))
+
+hotdogs = [train_imgs[i][0] for i in range(8)]
+not_hotdogs = [train_imgs[-i - 1][0] for i in range(8)]
+d2l.show_images(hotdogs + not_hotdogs, 2, 8, scale=1.4)
