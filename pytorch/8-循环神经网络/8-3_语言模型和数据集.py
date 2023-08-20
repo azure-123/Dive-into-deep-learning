54f2c77185f0f14ab960b54e961af080e26cdbc8
azure <1666534783@qq.com>
2023-08-20 11:14:51 +0800

    modify language models and dataset

--- pytorch/8-循环神经网络/8-3_语言模型和数据集.py
+++ pytorch/8-循环神经网络/8-3_语言模型和数据集.py
@@ -1,1 +1,8 @@
+import random
 import torch
+from d2l import torch as d2l
+
+tokens = d2l.tokenize(d2l.read_time_machine())
+corpus = [token for line in tokens for token in line]
+vocab = d2l.Vocab(corpus)
+print(vocab.token_freqs[:10])
